4127e145f34e040d6fa2168167ec1fc9a799393b
Tama THK <teguhpratama337@gmail.com>
2020-09-03 20:25:46 +0700

    series-1

---
+++ src/main/java/io/tam/ssc1/config/AppConfig.java
@@ -0,0 +1,33 @@
+package io.tam.ssc1.config;
+
+import org.springframework.cglib.proxy.NoOp;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+
+@Configuration
+public class AppConfig {
+
+    @Bean
+    public UserDetailsService userDetailsService() {
+        var uds = new InMemoryUserDetailsManager();
+
+        var u1 = User.withUsername("user")
+                     .password("pass")
+                     .authorities("read")
+                     .build();
+
+        uds.createUser(u1);
+
+        return uds;
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return NoOpPasswordEncoder.getInstance();
+    }
+}
---
+++ src/test/java/io/tam/ssc1/SsC1ApplicationTests.java
@@ -0,0 +1,13 @@
+package io.tam.ssc1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SsC1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
