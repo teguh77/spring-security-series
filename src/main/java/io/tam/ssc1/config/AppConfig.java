8dbb6810e4ed0debfeb95c571c4e8a19104e3cc2
Tama THK <teguhpratama337@gmail.com>
2020-09-03 22:59:39 +0700

    serie-2

--- src/main/java/io/tam/ssc1/config/AppConfig.java
+++ src/main/java/io/tam/ssc1/config/AppConfig.java
@@ -1,29 +1,25 @@
 package io.tam.ssc1.config;
 
-import org.springframework.cglib.proxy.NoOp;
+import io.tam.ssc1.repositories.UserRepository;
+import io.tam.ssc1.services.JPAUserDetailsService;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
 @Configuration
 public class AppConfig {
 
+    private final UserRepository userRepository;
+
+    public AppConfig(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
     @Bean
     public UserDetailsService userDetailsService() {
-        var uds = new InMemoryUserDetailsManager();
-
-        var u1 = User.withUsername("user")
-                     .password("pass")
-                     .authorities("read")
-                     .build();
-
-        uds.createUser(u1);
-
-        return uds;
+        return new JPAUserDetailsService(userRepository);
     }
 
     @Bean
--- src/main/java/io/tam/ssc1/controller/HelloController.java
+++ src/main/java/io/tam/ssc1/controller/HelloController.java
@@ -4,10 +4,11 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-public class TestController {
+public class HelloController {
 
-    @GetMapping("/test")
-    public String test() {
-        return "test";
+    @GetMapping("/hello")
+    public String hello() {
+        return "Hello!";
     }
+
 }
