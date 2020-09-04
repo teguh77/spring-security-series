4af8fedd8324bbac0369946085908e3136fd468f
Tama THK <teguhpratama337@gmail.com>
2020-09-04 11:33:12 +0700

    serie-3

--- src/main/java/io/tam/ssc1/config/AppConfig.java
+++ src/main/java/io/tam/ssc1/config/AppConfig.java
@@ -1,29 +1,48 @@
 package io.tam.ssc1.config;
 
-import io.tam.ssc1.repositories.UserRepository;
-import io.tam.ssc1.services.JPAUserDetailsService;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
+
+import javax.sql.DataSource;
 
 @Configuration
-public class AppConfig {
-
-    private final UserRepository userRepository;
-
-    public AppConfig(UserRepository userRepository) {
-        this.userRepository = userRepository;
-    }
+public class AppConfig extends WebSecurityConfigurerAdapter {
 
     @Bean
-    public UserDetailsService userDetailsService() {
-        return new JPAUserDetailsService(userRepository);
+    public JdbcUserDetailsManager jdbcUserDetailsManager() {
+        return new JdbcUserDetailsManager(dataSource());
     }
 
     @Bean
     public PasswordEncoder passwordEncoder() {
-        return NoOpPasswordEncoder.getInstance();
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public DataSource dataSource() {
+        DriverManagerDataSource dataSource = new DriverManagerDataSource();
+        dataSource.setUrl("jdbc:mysql://localhost/spring");
+        dataSource.setUsername("root");
+        dataSource.setPassword("mysql");
+
+        return dataSource;
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.httpBasic();
+
+        http.csrf().disable();
+
+        http.authorizeRequests()
+                .mvcMatchers("/user").permitAll()
+                .anyRequest().authenticated();
     }
 }
--- src/main/java/io/tam/ssc1/model/User.java
+++ src/main/java/io/tam/ssc1/model/User.java
@@ -1,33 +1,35 @@
-package io.tam.ssc1.services;
+package io.tam.ssc1.model;
 
-import io.tam.ssc1.entities.User;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
 import java.util.Collection;
 import java.util.List;
 
-public class AppUserDetails implements UserDetails {
+public class User implements UserDetails {
 
-    private final User user;
+    private String username;
+    private String password;
 
-    public AppUserDetails(User user) {
-        this.user = user;
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
     }
 
     @Override
     public Collection<? extends GrantedAuthority> getAuthorities() {
         return List.of(() -> "read");
-    }
-
-    @Override
-    public String getPassword() {
-        return user.getPassword();
-    }
-
-    @Override
-    public String getUsername() {
-        return user.getUsername();
     }
 
     @Override
