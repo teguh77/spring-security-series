a0ee868680ab33573f9ef98a6181194818c44568
Tama THK <teguhpratama337@gmail.com>
2020-09-04 18:44:48 +0700

    authProvider

--- src/main/java/io/tam/ssc1/config/AppConfig.java
+++ src/main/java/io/tam/ssc1/config/AppConfig.java
@@ -1,48 +1,45 @@
 package io.tam.ssc1.config;
 
+import io.tam.ssc1.security.CustomAuthenticationProvider;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.jdbc.datasource.DriverManagerDataSource;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.provisioning.JdbcUserDetailsManager;
-
-import javax.sql.DataSource;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
 @Configuration
-public class AppConfig extends WebSecurityConfigurerAdapter {
+public class AppConfig
+        extends WebSecurityConfigurerAdapter {
+
+    @Autowired
+    private CustomAuthenticationProvider customAuthenticationProvider;
 
     @Bean
-    public JdbcUserDetailsManager jdbcUserDetailsManager() {
-        return new JdbcUserDetailsManager(dataSource());
+    public UserDetailsService userDetailsService() {
+        InMemoryUserDetailsManager uds = new InMemoryUserDetailsManager();
+        UserDetails user = User.withUsername("john")
+                        .password("pass")
+                        .authorities("read")
+                        .build();
+        uds.createUser(user);
+
+        return uds;
     }
 
     @Bean
     public PasswordEncoder passwordEncoder() {
-        return new BCryptPasswordEncoder();
-    }
-
-    @Bean
-    public DataSource dataSource() {
-        DriverManagerDataSource dataSource = new DriverManagerDataSource();
-        dataSource.setUrl("jdbc:mysql://localhost/spring");
-        dataSource.setUsername("root");
-        dataSource.setPassword("mysql");
-
-        return dataSource;
+        return NoOpPasswordEncoder.getInstance();
     }
 
     @Override
-    protected void configure(HttpSecurity http) throws Exception {
-        http.httpBasic();
-
-        http.csrf().disable();
-
-        http.authorizeRequests()
-                .mvcMatchers("/user").permitAll()
-                .anyRequest().authenticated();
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        // bisa chaining authenticationprovider lebih dari satu
+        auth.authenticationProvider(customAuthenticationProvider);
     }
 }
--- src/main/java/io/tam/ssc1/controller/HelloController.java
+++ src/main/java/io/tam/ssc1/controller/HelloController.java
@@ -1,34 +1,14 @@
 package io.tam.ssc1.controller;
 
-import io.tam.ssc1.model.User;
-import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.provisioning.JdbcUserDetailsManager;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloController {
 
-    private final JdbcUserDetailsManager jdbcUserDetailsManager;
-    private final PasswordEncoder passwordEncoder;
-
-    public HelloController(JdbcUserDetailsManager jdbcUserDetailsManager,
-                           PasswordEncoder passwordEncoder) {
-        this.jdbcUserDetailsManager = jdbcUserDetailsManager;
-        this.passwordEncoder = passwordEncoder;
-    }
-
     @GetMapping("/hello")
     public String hello() {
         return "Hello!";
-    }
-
-    @PostMapping("/user")
-    public void addUser(@RequestBody User user) {
-        user.setPassword(passwordEncoder.encode(user.getPassword()));
-        jdbcUserDetailsManager.createUser(user);
     }
 
 }
