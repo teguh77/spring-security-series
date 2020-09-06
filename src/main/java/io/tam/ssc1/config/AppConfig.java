f4eb881bff6515738325b5c29ad74df199ce244d
Tama THK <teguhpratama337@gmail.com>
2020-09-06 23:08:44 +0700

    8-cors

--- src/main/java/io/tam/ssc1/config/AppConfig.java
+++ src/main/java/io/tam/ssc1/config/AppConfig.java
@@ -1,30 +1,35 @@
 package io.tam.ssc1.config;
 
-import io.tam.ssc1.security.CustomCsrfFilter;
+import org.apache.tomcat.util.file.ConfigurationSource;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.web.csrf.CsrfFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+
+import java.util.List;
+import java.util.concurrent.Callable;
 
 @Configuration
 public class AppConfig extends WebSecurityConfigurerAdapter {
 
-    private final CustomCsrfFilter customCsrfFilter;
-
-    public AppConfig(CustomCsrfFilter customCsrfFilter) {
-        this.customCsrfFilter = customCsrfFilter;
-    }
-
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        super.configure(http);
-//        http.csrf().disable();
-//        http.csrf(c -> {
-//            // customize csrf behaviour by override it self
-//            c.csrfTokenRepository(customCsrfTokenRepository);
-//        });
+        http.csrf().disable();
+        http.authorizeRequests()
+                .anyRequest()
+                .permitAll();
 
-        http.addFilterAfter(customCsrfFilter, CsrfFilter.class);
+        http.cors(c -> {
+            CorsConfigurationSource  cs = r -> {
+                CorsConfiguration cc = new CorsConfiguration();
+                cc.setAllowedOrigins(List.of("*"));
+                cc.setAllowedMethods(List.of("GET", "POST"));
+                // lebih baik spesify value of list dengan setting environtment variable
+                return cc;
+            };
+            c.configurationSource(cs);
+        });
 
     }
 }
--- src/main/java/io/tam/ssc1/controller/MainController.java
+++ src/main/java/io/tam/ssc1/controller/MainController.java
@@ -1,8 +1,10 @@
 package io.tam.ssc1.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class MainController {
@@ -12,10 +14,20 @@ public class MainController {
         return "main.html";
     }
 
-    @PostMapping("/change")
+    @PostMapping("/test")
+    @ResponseBody
+//    @CrossOrigin("*") not recommended
     public String change() {
+
+        // cors hanya mencegah response ke client tapi tidak mencegah access to endpoint
+        // jadi kode yang ada disini akan tetap diesekusi tetapi tidak akan me return response ke client karena di block
+
+        /*
+        ini akan dieksekusi meskipun cors origin blocked
         System.out.println(":(");
-        return "main.html";
+        */
+
+        return "TEST";
     }
 
 }
